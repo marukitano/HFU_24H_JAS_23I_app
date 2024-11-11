e006d424ccc51f199b68c52566a66fedc74113fb
maru <marcus.knop@outlook.com>
2024-11-11 21:07:34 +0100

    testfile angepasst

--- uebungen/sum.test.js
+++ uebungen/sum.test.js
@@ -1,3 +1,27 @@
 const sum= require('./sum');
 
-test('adds 1 + 2 to equal 3', () => { expect(sum(10, 2)).toBe(12); });
+test
+(
+    'adds 1 + 2 to equal 3', () =>
+    {
+        expect(sum(10, 2)).toBe(12);
+    }
+);
+
+test
+(
+    'array-test', () =>
+    {
+        const a = [1,2,3];
+        expect(a[0]).toBe(1);
+    }
+);
+
+test
+(
+    'Object-test', () =>
+    {
+        const a = { firstname: "bob"};
+        expect(a.firstname).toBe("bob");
+    }
+);
